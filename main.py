d5ce80b61defb8673902a9634b1f92ee7983be75
Justin-12138 <lz12138@mails.guet.edu.cn>
2023-11-05 14:03:03 +0800

    add req

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 data=pd.DataFrame(data=np.arange(16).reshape(4,4),columns=list('ABCD'),dtype=float,copy=True)
 print(data)
 b=data.iloc[0]
